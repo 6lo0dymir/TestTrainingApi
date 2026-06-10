66b8e860b5010295199b88e8293b4e7775847980
Vladimir <kennymc666@gmail.com>
2026-06-10 15:54:42 +0300

    Обновлены простые тесты REST добавлен cleaner, подчищены ненужные строки Тесты базы данных переписаны с учетом prepared statement

--- src/test/java/SimpleRestAssuredTests.java
+++ src/test/java/SimpleRestAssuredTests.java
@@ -18,13 +18,13 @@ import static org.hamcrest.Matchers.equalTo;
 public class SimpleRestAssuredTests {
     private static String baseUserPath;
     private static String baseContactPath;
-    private static Map <String,String> credentials;
+    private static Map<String, String> credentials;
     private static String token;
     private static String contactId;
 
 
     @BeforeAll
-    public static void setUp(){
+    public static void setUp() {
 
         String baseUrl = "https://thinking-tester-contact-list.herokuapp.com";
         baseUserPath = "/users";
@@ -52,7 +52,7 @@ public class SimpleRestAssuredTests {
         contactId = given()
                 .basePath(baseContactPath)
                 .accept(ContentType.JSON)
-                .header("Authorization","Bearer "+token)
+                .header("Authorization", "Bearer " + token)
                 .when()
                 .get()
                 .then()
@@ -61,22 +61,71 @@ public class SimpleRestAssuredTests {
                 .path("find {it.email == '" + targetEmail + "'}._id");
 
     }
+    @AfterEach
+    public void cleanUp() {
+        if (contactId != null) {
+            deleteContact(contactId);
+            contactId = null;
+        }
+    }
+
+    @Step("Создание нового тела контакта")
+    private Map<String,String> createContactBody(){
+        Map<String, String> newContact = new HashMap<>();
+        newContact.put("firstName", "John");
+        newContact.put("lastName", "Doe");
+        newContact.put("birthdate", "1990-01-01");
+        newContact.put("email", "john.doe@example.com");
+        newContact.put("phone", "88005553535");
+        newContact.put("street1", "123 Main St");
+        newContact.put("street2", "Apt 4B");
+        newContact.put("city", "New York");
+        newContact.put("stateProvince", "NY");
+        newContact.put("postalCode", "10001");
+        newContact.put("country", "USA");
+        return newContact;
+    }
+    @Step("Создание нового контакта с извлечением id")
+    private String createTestContact(){
+        Map<String,String> contact = createContactBody();
+        return given()
+                .basePath(baseContactPath)
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer " + token)
+                .body(contact)
+                .when()
+                .post()
+                .then()
+                .extract()
+                .path("_id");
+    }
+    @Step("Удаление контакта по ID")
+    private void deleteContact(String contactId) {
+        if (contactId != null) {
+            try {
+                given()
+                        .basePath(baseContactPath)
+                        .header("Authorization", "Bearer " + token)
+                        .delete("/" + contactId);
+            } catch (Exception e) {
+                System.err.println("Cleanup failed for " + contactId + ": " + e.getMessage());
+            }
+        }
+    }
+
+
     @DisplayName("Базовая аутентификация")
     @Test
     @Order(1)
-    public void authBaseTest(){
-        Map<String, String> credentials = Map.of(
-                "email", "kennymc666@gmail.com",
-                "password", "12345678"
-        );
+    public void authBaseTest() {
         given()
                 .basePath(baseUserPath)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .body(credentials)
-                    .when()
+                .when()
                 .post("/login")
-                    .then()
+                .then()
                 .statusCode(200)
                 .body("token", notNullValue())
                 .body("user", notNullValue())
@@ -87,7 +136,7 @@ public class SimpleRestAssuredTests {
     @DisplayName("Получение данных пользователя с токеном авторизации")
     @Test
     @Order(2)
-    public void getUserDataWithToken(){
+    public void getUserDataWithToken() {
         given()
                 .basePath(baseUserPath)
                 .header("Authorization", "Bearer " + token)
@@ -100,41 +149,30 @@ public class SimpleRestAssuredTests {
                 .body("email", equalTo("kennymc666@gmail.com"))
                 .body("firstName", notNullValue())
                 .body("lastName", notNullValue())
-                ;
+        ;
     }
+
     @DisplayName("Добавление нового контакта")
     @Test
     @Order(3)
-    public void addNewContact(){
-        Map<String,String> newContact = new HashMap<>();
-        newContact.put("firstName", "John");
-        newContact.put("lastName", "Doe");
-        newContact.put("birthdate", "1990-01-01");
-        newContact.put("email", "john.doe@example.com");
-        newContact.put("phone", "88005553535");
-        newContact.put("street1", "123 Main St");
-        newContact.put("street2", "Apt 4B");
-        newContact.put("city", "New York");
-        newContact.put("stateProvince", "NY");
-        newContact.put("postalCode", "10001");
-        newContact.put("country", "USA");
-
-
+    public void addNewContact() {
+        Map<String,String> contact = createContactBody();
+        String contactID = createTestContact();
         given()
                 .basePath(baseContactPath)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
-                .header("Authorization", "Bearer "+token)
-                .body(newContact)
+                .header("Authorization", "Bearer " + token)
+                .body(contact)
                 .when()
                 .post("")
                 .then()
                 .statusCode(201)
                 .body("_id", notNullValue())
-                .body("firstName", equalTo(newContact.get("firstName")))
-                .body("lastName", equalTo(newContact.get("lastName")))
-                .body("email",equalTo(newContact.get("email")))
-                .body("phone",equalTo(newContact.get("phone")))
+                .body("firstName", equalTo(contact.get("firstName")))
+                .body("lastName", equalTo(contact.get("lastName")))
+                .body("email", equalTo(contact.get("email")))
+                .body("phone", equalTo(contact.get("phone")))
                 .extract()
                 .path("_id");
 
@@ -143,12 +181,13 @@ public class SimpleRestAssuredTests {
     @DisplayName("Находим id контакта по email")
     @Test
     @Order(4)
-    public void getContactIdByEmail(){
+    public void getContactIdByEmail() {
+        createTestContact();
         String targetEmail = "john.doe@example.com";
         String contactId = given()
                 .basePath(baseContactPath)
                 .accept(ContentType.JSON)
-                .header("Authorization","Bearer "+token)
+                .header("Authorization", "Bearer " + token)
                 .when()
                 .get()
                 .then()
@@ -159,51 +198,52 @@ public class SimpleRestAssuredTests {
         given()
                 .basePath(baseContactPath)
                 .accept(ContentType.JSON)
-                .header("Authorization", "Bearer "+token)
+                .header("Authorization", "Bearer " + token)
                 .when()
-                .get("/"+contactId)
+                .get("/" + contactId)
                 .then()
                 .statusCode(200)
-                .body("email",equalTo(targetEmail));
+                .body("email", equalTo(targetEmail));
     }
+
     @DisplayName("Изменяем данные контакта метод PATCH")
     @Test
     @Order(5)
-    public void changeContactData(){
-        Map<String,String> newContactData = Map.of(
-             "firstName","Жека",
+    public void changeContactData() {
+        String contactId = createTestContact();
+        Map<String, String> newContactData = Map.of(
+                "firstName", "Жека",
                 "lastName", "Неизвестный"
         );
         given()
                 .basePath(baseContactPath)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
-                .header("Authorization","Bearer "+token)
+                .header("Authorization", "Bearer " + token)
                 .body(newContactData)
                 .when()
-                .patch("/"+contactId)
+                .patch("/" + contactId)
                 .then()
                 .statusCode(200)
-                .body("firstName",equalTo(newContactData.get("firstName")))
-                .body("lastName",equalTo(newContactData.get("lastName")))
-                ;
+                .body("firstName", equalTo(newContactData.get("firstName")))
+                .body("lastName", equalTo(newContactData.get("lastName")))
+        ;
     }
+
     @DisplayName("Удаление контакта по ID")
     @Test
     @Order(6)
-    public void deleteContactById(){
+    public void deleteContactById() {
         given()
                 .basePath(baseContactPath)
                 .accept(ContentType.JSON)
-                .header("Authorization","Bearer "+token)
+                .header("Authorization", "Bearer " + token)
                 .log().all()
                 .when()
-                .delete("/"+contactId)
+                .delete("/" + contactId)
                 .then()
                 .log().all()
                 .statusCode(200);
     }
-
-
-    }
+}
 
--- src/test/java/dataBase/test/SimpleSQLRequests.java
+++ src/test/java/dataBase/test/SimpleSQLRequests.java
@@ -20,9 +20,10 @@ public class SimpleSQLRequests extends BaseDBTest {
     @DisplayName("Проверка подключения к базе данных")
     @Test
     public void authenticationServiceConnectionTest() throws SQLException, IOException {
+        String sql = "SELECT now()";
         try (Connection conn = connection(DBList.Database.DB_AUTHENTICATION_SERVICE_TEST);
-             Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery("SELECT now()")) {
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
 
             if (rs.next()) {
                 String currTime = rs.getString(1);
@@ -35,10 +36,13 @@ public class SimpleSQLRequests extends BaseDBTest {
     @DisplayName("Получение UID по email")
     @Test
     public void getUIDByEmail() throws SQLException, IOException{
+        String targetEmail = "a.petrov@mail.ru";
+        String sql = "SELECT id FROM client WHERE email = ?";
         try (Connection conn = connection(DBList.Database.DB_AUTHENTICATION_SERVICE_TEST);
-        Statement stmt = conn.createStatement();
-        ResultSet rst = stmt.executeQuery("SELECT id FROM client WHERE email = 'a.petrov@mail.ru'")){
-
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        ){
+            pstmt.setString(1,targetEmail);
+            ResultSet rst = pstmt.executeQuery();
             if (rst.next()){
                 String uID = rst.getString(1);
                 System.out.println("UUID: " + uID);
@@ -51,11 +55,12 @@ public class SimpleSQLRequests extends BaseDBTest {
     @DisplayName("Отображение названия и описания всех актуальных карточных продуктов (дебет)")
     @Test
     public void getListOfActualDebetCards() throws SQLException, IOException{
+        String sql = "SELECT name, description FROM product WHERE status = ?";
         try (Connection conn = connection(DBList.Database.DB_CARD_SERVICE_TEST);
-             Statement stmt = conn.createStatement();
-             ResultSet rst = stmt.executeQuery("SELECT name, description " +
-                     "FROM product " +
-                     "WHERE status IS true ")){
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ){
+            pstmt.setBoolean(1,true);
+            ResultSet rst = pstmt.executeQuery();
             while (rst.next()){
                 String name = rst.getString("name");
                 String description = rst.getString("description");
@@ -66,12 +71,14 @@ public class SimpleSQLRequests extends BaseDBTest {
     @DisplayName("Получение списка активных счетов по типу (Debit, Credit, Deposit")
     @Test
     public void getListOfActualAccountsByType() throws SQLException, IOException{
+        String sql = "SELECT account_number, type FROM account a JOIN type_account ta " +
+                "ON a.type_account_id = ta.id WHERE a.account_status = ? ORDER BY ta.type ASC";
+        String status = "ACTIVE";
         try (Connection conn = connection(DBList.Database.DB_ACCOUNT_SERVICE_TEST);
-        Statement stmt = conn.createStatement();
-        ResultSet rst = stmt.executeQuery("SELECT account_number, type " +
-                "FROM account a " +
-                "JOIN type_account ta ON a.type_account_id = ta.id " +
-                "WHERE a.account_status = 'ACTIVE'")){
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        ){
+            pstmt.setString(1,status);
+            ResultSet rst = pstmt.executeQuery();
             while (rst.next()){
                 String acc_number = rst.getString("account_number");
                 String type = rst.getString("type");
